0a4edcf3eeabe1fe5fbfef5391dc5a56ad0e6a28
rds2151 <rds@localhost.localdomain>
2024-05-16 18:04:27 +0530

    added filter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {data: null});
 });
+
+router.post('/filter', (req, res) => {
+  data = {
+    location : req.body.location,
+    source: req.body.source,
+    destination: req.body.destination,
+    neighbour: req.body.neighbour
+  };
+
+  res.render('index', {data: data})
+})
 
 module.exports = router;
